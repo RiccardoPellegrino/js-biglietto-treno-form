d85fc8b7816373d870874d201f1ddfda2dcdf539
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-10-07 17:14:50 +0200

    scaffolding fine

--- js/script.js
+++ js/script.js
@@ -11,3 +11,38 @@
 // MILESTONE 2:
 // Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 // Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+
+let bottone = document.getElementById('calcola');
+bottone.addEventListener("click", function () {
+    let klm = document.getElementById('klm').value;
+    let eta = document.getElementById('eta').value;
+    let prezzoBiglietto = klm * 0.21;
+    let scontoBiglietto;
+    console.log(klm);
+    console.log(eta);
+    console.log(prezzoBiglietto);
+    console.log(scontoBiglietto);
+
+    if (isNaN(klm) || isNaN(eta)) {
+        alert('Attenzione devi inserire solo numeri e non caratteri');
+        window.location.reload();
+    }
+    if (eta < 18) {
+        scontoBiglietto = (prezzoBiglietto *80) /100;
+        console.log(scontoBiglietto);
+        scontoBiglietto = scontoBiglietto.toFixed(2);
+        document.getElementById("prezzo").innerHTML = scontoBiglietto + ' &euro;';
+
+    } else if (eta > 65) {
+        scontoBiglietto = (prezzoBiglietto *60) /100;
+        console.log(scontoBiglietto);
+        scontoBiglietto = scontoBiglietto.toFixed(2);
+        document.getElementById("prezzo").innerHTML = scontoBiglietto + ' &euro;';
+    } else {
+        scontoBiglietto = prezzoBiglietto;
+        console.log(scontoBiglietto);
+        scontoBiglietto = scontoBiglietto.toFixed(2);
+        document.getElementById("prezzo").innerHTML = scontoBiglietto + ' &euro;';
+    }
+
+});
